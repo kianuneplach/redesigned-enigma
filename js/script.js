49212097facc0208d4e0f9a83d832a2a96480809
Maria <kianuneplach@yandex.ru>
2020-08-30 17:01:36 +0500

    try this bitch

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+
+
+//const result = confirm("Are you here?");
+
+//const answer=prompt("are you 18?", "18"); 
+//console.log(typeof(answer));
+
+const answers = [];
+
+//answers[0] = prompt('What is your name', '');
+//answers[1] = prompt('What is your last name', '');
+//answers[2] = prompt('What is your age', '');
+
+
+console.log(typeof(answers));
